b244071d1f433eede50cd1788a8318a89116bf02
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-25 12:20:44 -0700

    updated copyright

--- 2unix.sh
+++ 2unix.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+
+# Copyright (c) 2018 Michael Neill Hartman. All rights reserved.
+# mnh_license@proton.me
+# https://github.com/hartmanm
+
 sudo chown m1 /media/ramdisk
 sleep 2
-# download 1bash to ramdisk
-#DL=$(/usr/bin/curl "https://openrig.net/1bash")
-#cat <<EOF >/media/ramdisk/1bash
-#$DL
-#EOF
-# exe
 cp -rp /media/m1/0rig_here_1/1bash.sh /media/ramdisk/1bash.sh
 pkill -e 1bash
 pkill -f 1bash
 until `bash /media/ramdisk/1bash.sh`
 do
 sleep 4
-done ## until `bash /media/ramdisk/1bash`
+done
 # xterm -e /home/m1/2unix -geometry 100x80+300+300
